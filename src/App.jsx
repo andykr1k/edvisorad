215510aa634e3a05c2d6a3dfe72889d0fe86f34f
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-09-23 18:31:46 -0700

    Changed Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import logo from './assets/front.png'
+import bg from './assets/bg.png'
 import {
   Alert,
   AlertIcon,
@@ -16,21 +17,40 @@ function App() {
 
   return (
     <div className="App">
+
+    
+
+    <div className='fixed z-1 bg-transparent min-h-screen min-w-full'> 
       <div className='flex align-middle justify-center p-10 pb-2'>
-        <h1 className='text-3xl md:text-4xl font-bold'>Coming Soon!</h1>
+        <h1 className='text-3xl md:text-4xl font-bold text-white'>Coming Soon!</h1>
       </div>
       <div className='flex align-middle justify-center p-1'>
-        <h2 className='text-sm md:text-1xl'>Streamline your college transfer and graduation process.</h2>        
+        <h2 className='text-sm md:text-1xl text-white'>Streamline your college transfer and graduation process.</h2>        
       </div>
 
-      <div className='grid place-items-center'>
+      <div className='grid place-items-center pt-10 md:hidden'>
         <Image src={logo}  
           boxSize='500px'
           objectFit='cover'
+          className='bg-none'
         />
       </div>
 
-      <div className='fixed bottom-0 left-0 right-0 pl-10 pb-10 pr-10 flex items-baseline'>
+      <div className='place-items-center pt-10 hidden md:grid'>
+        <Image src={logo}  
+          boxSize='600px'
+          objectFit='cover'
+          className='bg-none'
+        />
+      </div>
+
+    </div>
+      
+    <div className='min-h-screen min-w-full z-0'>
+      <img src={bg} className='min-h-screen min-w-full'/>
+    </div>
+    
+      <div className='fixed z-1 bottom-0 left-0 right-0 pl-10 pb-10 pr-10 flex items-baseline bg-transparent'>
         <Input 
           focusBorderColor='blue.400' 
           variant='flushed' 
@@ -40,7 +60,7 @@ function App() {
         <div className='ml-5'>
         <Button
             variant='outline'
-            colorScheme='blue'
+            colorScheme='whiteAlpha'
             onClick={() =>
               toast({
                 title: 'You have been added to our newsletter.',
